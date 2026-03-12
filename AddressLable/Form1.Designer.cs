c29bc5ce97a905ae52b952408f8405f8023e198f
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-03-12 07:23:09 -0600

    Add project files.

---
+++ AddressLable/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AddressLable
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FirstNameTextBox = new TextBox();
+            LastNameTextBox = new TextBox();
+            StreetTextBox = new TextBox();
+            CityTextBox = new TextBox();
+            StateTextBox = new TextBox();
+            ZipTextBox = new TextBox();
+            FirstNameLabel = new Label();
+            LastNameLabel = new Label();
+            StreetLabel = new Label();
+            CityLabel = new Label();
+            StateLabel = new Label();
+            ZipLabel = new Label();
+            InfoGroupBox = new GroupBox();
+            DisplayButton = new Button();
+            ClearButton = new Button();
+            ExitButton = new Button();
+            InfoGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // FirstNameTextBox
+            // 
+            FirstNameTextBox.Location = new Point(128, 24);
+            FirstNameTextBox.Name = "FirstNameTextBox";
+            FirstNameTextBox.Size = new Size(150, 31);
+            FirstNameTextBox.TabIndex = 0;
+            // 
+            // LastNameTextBox
+            // 
+            LastNameTextBox.Location = new Point(128, 61);
+            LastNameTextBox.Name = "LastNameTextBox";
+            LastNameTextBox.Size = new Size(150, 31);
+            LastNameTextBox.TabIndex = 1;
+            // 
+            // StreetTextBox
+            // 
+            StreetTextBox.AccessibleRole = AccessibleRole.PageTabList;
+            StreetTextBox.Location = new Point(128, 98);
+            StreetTextBox.Name = "StreetTextBox";
+            StreetTextBox.Size = new Size(150, 31);
+            StreetTextBox.TabIndex = 2;
+            // 
+            // CityTextBox
+            // 
+            CityTextBox.Location = new Point(128, 135);
+            CityTextBox.Name = "CityTextBox";
+            CityTextBox.Size = new Size(150, 31);
+            CityTextBox.TabIndex = 3;
+            // 
+            // StateTextBox
+            // 
+            StateTextBox.Location = new Point(128, 172);
+            StateTextBox.Name = "StateTextBox";
+            StateTextBox.Size = new Size(150, 31);
+            StateTextBox.TabIndex = 4;
+            // 
+            // ZipTextBox
+            // 
+            ZipTextBox.Location = new Point(128, 209);
+            ZipTextBox.Name = "ZipTextBox";
+            ZipTextBox.Size = new Size(150, 31);
+            ZipTextBox.TabIndex = 5;
+            // 
+            // FirstNameLabel
+            // 
+            FirstNameLabel.AutoSize = true;
+            FirstNameLabel.Location = new Point(6, 27);
+            FirstNameLabel.Name = "FirstNameLabel";
+            FirstNameLabel.Size = new Size(97, 25);
+            FirstNameLabel.TabIndex = 6;
+            FirstNameLabel.Text = "First Name";
+            // 
+            // LastNameLabel
+            // 
+            LastNameLabel.AutoSize = true;
+            LastNameLabel.Location = new Point(6, 64);
+            LastNameLabel.Name = "LastNameLabel";
+            LastNameLabel.Size = new Size(95, 25);
+            LastNameLabel.TabIndex = 7;
+            LastNameLabel.Text = "Last Name";
+            // 
+            // StreetLabel
+            // 
+            StreetLabel.AutoSize = true;
+            StreetLabel.Location = new Point(6, 101);
+            StreetLabel.Name = "StreetLabel";
+            StreetLabel.Size = new Size(127, 25);
+            StreetLabel.TabIndex = 8;
+            StreetLabel.Text = "Street Address";
+            // 
+            // CityLabel
+            // 
+            CityLabel.AutoSize = true;
+            CityLabel.Location = new Point(6, 138);
+            CityLabel.Name = "CityLabel";
+            CityLabel.Size = new Size(42, 25);
+            CityLabel.TabIndex = 9;
+            CityLabel.Text = "City";
+            // 
+            // StateLabel
+            // 
+            StateLabel.AutoSize = true;
+            StateLabel.Location = new Point(6, 175);
+            StateLabel.Name = "StateLabel";
+            StateLabel.Size = new Size(51, 25);
+            StateLabel.TabIndex = 10;
+            StateLabel.Text = "State";
+            // 
+            // ZipLabel
+            // 
+            ZipLabel.AutoSize = true;
+            ZipLabel.Location = new Point(6, 212);
+            ZipLabel.Name = "ZipLabel";
+            ZipLabel.Size = new Size(84, 25);
+            ZipLabel.TabIndex = 11;
+            ZipLabel.Text = "Zip Code";
+            // 
+            // InfoGroupBox
+            // 
+            InfoGroupBox.Controls.Add(FirstNameLabel);
+            InfoGroupBox.Controls.Add(ZipLabel);
+            InfoGroupBox.Controls.Add(FirstNameTextBox);
+            InfoGroupBox.Controls.Add(StateLabel);
+            InfoGroupBox.Controls.Add(LastNameTextBox);
+            InfoGroupBox.Controls.Add(CityLabel);
+            InfoGroupBox.Controls.Add(StreetTextBox);
+            InfoGroupBox.Controls.Add(StreetLabel);
+            InfoGroupBox.Controls.Add(CityTextBox);
+            InfoGroupBox.Controls.Add(LastNameLabel);
+            InfoGroupBox.Controls.Add(StateTextBox);
+            InfoGroupBox.Controls.Add(ZipTextBox);
+            InfoGroupBox.Location = new Point(12, 12);
+            InfoGroupBox.Name = "InfoGroupBox";
+            InfoGroupBox.Size = new Size(290, 254);
+            InfoGroupBox.TabIndex = 12;
+            InfoGroupBox.TabStop = false;
+            InfoGroupBox.Text = "Info";
+            // 
+            // DisplayButton
+            // 
+            DisplayButton.Location = new Point(440, 364);
+            DisplayButton.Name = "DisplayButton";
+            DisplayButton.Size = new Size(112, 74);
+            DisplayButton.TabIndex = 13;
+            DisplayButton.Text = "&Display";
+            DisplayButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(558, 364);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(112, 74);
+            ClearButton.TabIndex = 14;
+            ClearButton.Text = "&Clear";
+            ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(676, 364);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(112, 74);
+            ExitButton.TabIndex = 15;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ExitButton);
+            Controls.Add(ClearButton);
+            Controls.Add(DisplayButton);
+            Controls.Add(InfoGroupBox);
+            Name = "Form1";
+            Text = "Form1";
+            InfoGroupBox.ResumeLayout(false);
+            InfoGroupBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox FirstNameTextBox;
+        private TextBox LastNameTextBox;
+        private TextBox StreetTextBox;
+        private TextBox CityTextBox;
+        private TextBox StateTextBox;
+        private TextBox ZipTextBox;
+        private Label FirstNameLabel;
+        private Label LastNameLabel;
+        private Label StreetLabel;
+        private Label CityLabel;
+        private Label StateLabel;
+        private Label ZipLabel;
+        private GroupBox InfoGroupBox;
+        private Button DisplayButton;
+        private Button ClearButton;
+        private Button ExitButton;
+    }
+}
---
+++ AddressLable/Form1.cs
@@ -0,0 +1,10 @@
+namespace AddressLable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
